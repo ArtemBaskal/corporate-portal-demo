7f01f225005df04aa61654bde94b00b8102a634e
ArtemBaskal <asbaskal@edu.hse.ru>
2019-09-11 01:18:21 +0300

    Selecting unique elements

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { connect } from "react-redux";
 
 const Card = ({ label, handleClick }) => (
   <div onClick={handleClick}>{label || "NoNameCard"}</div>
 );
 
-const mapStateToProps = state => ({
-  catalog: state.catalog
-});
-
-export default connect(mapStateToProps)(Card);
+export default Card;
--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -5,7 +5,7 @@ const INITAL_STATE = [1, 2, 3, 4, 5];
 export default (state = INITAL_STATE, action) => {
   switch (action.type) {
     case SELECT_FROM_CATALOG:
-      return [...state, action.payload];
+      if (!state.includes(action.payload)) return [...state, action.payload];
     default:
       return state;
   }
