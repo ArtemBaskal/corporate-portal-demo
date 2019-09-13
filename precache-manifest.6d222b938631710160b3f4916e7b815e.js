7ff264d517f12de29b65c751fd4679c3cf545caf
ArtemBaskal <asbaskal@edu.hse.ru>
2019-09-14 01:02:19 +0300

    Updates

---
+++ precache-manifest.6d222b938631710160b3f4916e7b815e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da4e902939b8bc9b8446dfcaf0523e1",
+    "url": "/Corporate-Portal/index.html"
+  },
+  {
+    "revision": "c8344435aafd330fcf9d",
+    "url": "/Corporate-Portal/static/css/main.689ddf3d.chunk.css"
+  },
+  {
+    "revision": "70370195e37bfc9f0b5b",
+    "url": "/Corporate-Portal/static/js/2.87298e13.chunk.js"
+  },
+  {
+    "revision": "c8344435aafd330fcf9d",
+    "url": "/Corporate-Portal/static/js/main.79f24d51.chunk.js"
+  },
+  {
+    "revision": "8dd463e980084954a416",
+    "url": "/Corporate-Portal/static/js/runtime~main.131fd1fb.js"
+  }
+]);
