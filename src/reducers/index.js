c7f2410dae9703a44961e42aa2e56317f7a62949
ArtemBaskal <asbaskal@edu.hse.ru>
2019-09-18 22:16:48 +0300

    Typescript initial setup

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import selectedItemsReducer from "./selectedItemsReducer";
-import accessRightsReducer from "./accessRightsReducer";
-
-export default combineReducers({
-  item: selectedItemsReducer,
-  accessRights: accessRightsReducer
-});
