632f74af219778cc398ac77fcee40cd3ff7e6478
ArtemBaskal <asbaskal@edu.hse.ru>
2019-09-22 00:23:46 +0300

    Updates

--- precache-manifest.64d7e4f312579b58ffd10b18865810f9.js
+++ precache-manifest.64d7e4f312579b58ffd10b18865810f9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d531e73307a4ffd90c95c52832ecd245",
+    "revision": "a9cffe540b6fc446b81d60ab3d4f244b",
     "url": "/Corporate-Portal/index.html"
   },
   {
-    "revision": "e2530af6faa68c1e7580",
+    "revision": "2f9a616091a4677410f9",
     "url": "/Corporate-Portal/static/css/main.19dfc229.chunk.css"
   },
   {
-    "revision": "1fd52c9288ca36e80c05",
-    "url": "/Corporate-Portal/static/js/2.76e9ad17.chunk.js"
+    "revision": "c603aab17ee0e7b754b6",
+    "url": "/Corporate-Portal/static/js/2.3bf95d42.chunk.js"
   },
   {
-    "revision": "e2530af6faa68c1e7580",
-    "url": "/Corporate-Portal/static/js/main.bf5c199c.chunk.js"
+    "revision": "2f9a616091a4677410f9",
+    "url": "/Corporate-Portal/static/js/main.57d49f82.chunk.js"
   },
   {
     "revision": "8dd463e980084954a416",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Corporate-Portal/precache-manifest.cd3473b59b66b4b2eeebeccb6a207092.js"
+  "/Corporate-Portal/precache-manifest.64d7e4f312579b58ffd10b18865810f9.js"
 );
 
 self.addEventListener('message', (event) => {
